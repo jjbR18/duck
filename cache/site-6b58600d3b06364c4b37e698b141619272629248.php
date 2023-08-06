fc7e090b6f1f29ebd17cf65d720ff133bb7ed977
jjbR18 <64841468+jjbR18@users.noreply.github.com>
2023-08-06 11:59:51 +0800

    Add files via upload

---
+++ cache/site-6b58600d3b06364c4b37e698b141619272629248.php
@@ -0,0 +1,69 @@
+<?php
+ return array (
+  'name' => 'wtf.cc',
+  'logo' => '',
+  'loginPluginId' => '102',
+  'enableCreateGroup' => '1',
+  'enableAddFriendInGroup' => '1',
+  'groupQRCodeExpireTime' => '0',
+  'maxGroupMembers' => '100',
+  'enableAddFriend' => '1',
+  'enableTmpChat' => '1',
+  'enableInvitationCode' => '0',
+  'enableRealName' => '0',
+  'openSSL' => '0',
+  'openWebEdition' => '1',
+  'enableWebWidget' => '0',
+  'sitePubkPem' => '-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAubRICosJ22iUC+M3UIU+
+h8zLF9nGJ4MbtUvvGBms3lz70kbaDp8N781MsKx3YAXlod6ehRbIPjQ2+Hq57C5I
+QnfazXUQJKlVlYveXcCkoqs0reA8g5AJzKs5TjVgqaJ+kGItKh1LDbRn5sqynxos
+IytYWWqkbfTJGPX0GJQ3CayS3IRP42fssoPPsS2V4a1QXdi+wgqb8Ez1p7hJ2I+r
+Z2eWbowbr+lTEARE1bfc91fLgcj92vsHj5VEcstl8C4Nk1n3s4GGPHWq4WLuZGgO
+QRBHy9aNJqJKBYBWBg+rbmj8cEVKYN9WV+2mlyDiowEEGXSnhf9/vmaIWF1dNMhu
+YwIDAQAB
+-----END PUBLIC KEY-----',
+  'sitePrikPem' => '-----BEGIN PRIVATE KEY-----
+MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC5tEgKiwnbaJQL
+4zdQhT6HzMsX2cYngxu1S+8YGazeXPvSRtoOnw3vzUywrHdgBeWh3p6FFsg+NDb4
+ernsLkhCd9rNdRAkqVWVi95dwKSiqzSt4DyDkAnMqzlONWCpon6QYi0qHUsNtGfm
+yrKfGiwjK1hZaqRt9MkY9fQYlDcJrJLchE/jZ+yyg8+xLZXhrVBd2L7CCpvwTPWn
+uEnYj6tnZ5ZujBuv6VMQBETVt9z3V8uByP3a+wePlURyy2XwLg2TWfezgYY8darh
+Yu5kaA5BEEfL1o0mokoFgFYGD6tuaPxwRUpg31ZX7aaXIOKjAQQZdKeF/3++ZohY
+XV00yG5jAgMBAAECggEBALdC/XFCIvkerDNLCNRymfHkHWytIgruLNLuW8aZ1+od
+Mvohp6GUP2muXYdgu/HMAACNNDy87eHNH455dq9oQTEvHINzifnUDjylqk01BoPt
+lvAcgTuIUtNjCgtvS7Q8QtBzwoR0IhQlPAAbesjeSD5w0SB0yE4mqqnyVvBGnEXz
+FBoo3jcWQkSWROi6VZSYnNdgY63X+h4S5rJ68+XWgoz0Wc/2WcpqrMGELmwwRXak
+9Ft05vVVKGEQW/f8jgGvU1G0O4PACifHQ3DRpo5/6tS5uBgkbAigZpO7n0IVCZK5
+iq5IKdPJ7TtzefWZ0iOvE2/aGXoxTlMTg9k1JnbHebkCgYEA6c/PJkzSMnzBD10x
+f30UMUtRXgv5oH0iRjr+K6x5Tr5AZv0GuedDkvzhWi8iWMBJMnqG5NwZYQ/iQtEw
+Zl881moyvCOtG3lvELoifdloYfGWeJInseF3T1YlEUfs9SwRtFurc9xoPwmTMoG0
+JUKTmpFf4o+PWQ9FnxAWpvofJo8CgYEAy1PBJIGJdpuMjo+bXswvUcshAlyCHf5L
+mFazYyhQQwFnF50PqBZUjdDpPr8NHMxSMmGdvMqJfENtjwbP3F1POljXELfAIJ3W
+GtLsu6NqUajzL6EOAh3FCZ92nbq37HHR2ek/LoFxK15z5DqFuq7c/7U9HE/tWDpg
+ZuUF5ZDwhO0CgYEAr/oWoW8Ebbdz30p3LMtJXWigzzYYJfDI0S8pill/3AJZmqoq
+W9VyCLmIK0Z6hW4EKZgK20nFWEaKN1wXH5Xixkab9PqtGZamP2K4/xuY8s7QP+NR
+ORLHp9DgiUcnU8hdcu/j9YoYdAmS3lq86aZYTefI0U7Xx2I1Nmj3g6mRtIUCgYBv
+Nu2zUSNme+ii5cv/S0IqxCfXCdCmJT0mI25Vs2xLak4AbYlt0GEJfRbHmmrKsLvZ
+EOndpQ8UqDEaBYjWqTc32/YvD8MIVC0TWQHiFJ2H8xipmbd20t++xrlzxvjpXMyD
+HADSKD1FkpFjadtR5Gm/eFvny5QwKSYnwee6zM+ydQKBgEkGoLaUjldGqkKWxKlT
+Kabebc8v/m1aaCYnD+/YI/v1r7bei9OGFdPeiVuZq7phv+sEogEhy9GXHpTVlbmN
+W6qX4XoPeehsqblszUN9akuZBk67wTwjEPeFKwXPwcgDfcavZNN9JiILkzUkStnT
+ruv8ewTjodsxSAXv8oUv1NBs
+-----END PRIVATE KEY-----',
+  'pushType' => '1',
+  'maxClientsNum' => '200',
+  'defaultFriends' => '',
+  'defaultGroups' => '',
+  'enableShareUser' => '1',
+  'enableShareGroup' => '1',
+  'pluginPublicKey' => 'HQIhnNInMe1ehRTnsHV883QnIbDcwUtc',
+  'owner' => '650cb56513da23bbcea811b1f0054fd9fe556439',
+  'managers' => '',
+  'maxMobileNum' => '1',
+  'maxWebNum' => '1',
+  'maxFileSize' => '10',
+  'openWaterMark' => '0',
+  'passportAccountSafePluginId' => '105',
+);
+ 

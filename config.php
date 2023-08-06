cfff87e6412b2abb8c0d82880b5bcdb7a4f04b70
jjbR18 <64841468+jjbR18@users.noreply.github.com>
2023-08-06 11:58:00 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,41 @@
+<?php
+ return array (
+  'siteVersionName' => '1.1.7',
+  'siteVersionCode' => '10107',
+  'siteId' => '6b58600d3b06364c4b37e698b141619272629248',
+  'apiPageIndex' => './index.php?action=page.index',
+  'apiPageLogin' => './index.php?action=page.login',
+  'apiPageLogout' => './index.php?action=page.logout',
+  'apiPageJump' => './index.php?action=page.jump',
+  'loginPluginId' => 102,
+  'apiPageWidget' => './index.php?action=page.widget',
+  'apiPageSiteInit' => './index.php?action=installDB',
+  'sessionVerify102' => './index.php?action=api.session.verify&body_format=base64pb',
+  'testCurl' => './index.php?action=installDB&for=test_curl',
+  'apiPagePassportLogin' => './index.php?action=page.passport.login',
+  'errorLog' => 'php_errors_URW8a5M5qJDhwd4Q.log',
+  'dbType' => 'mysql',
+  'dbVersion' => '2',
+  'sqlite' => 
+  array (
+    'sqliteDBPath' => '.',
+    'sqliteDBName' => '',
+  ),
+  'mysql' => 
+  array (
+    'dbName' => 'test',
+    'dbHost' => 'localhost',
+    'dbPort' => '3306',
+    'dbUserName' => 'test',
+    'dbPassword' => '123456',
+  ),
+  'mysqlSlave' => 
+  array (
+  ),
+  'logPath' => '.',
+  'randomKey' => 'URW8a5M5qJDhwd4Q',
+  'debugMode' => false,
+  'msectime' => 1691293675630.0,
+  'siteAddress' => 'http://wtf.cc',
+);
+ 
